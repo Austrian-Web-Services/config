a9cff92071a6f7b64aa0ec41f75406401ca57f38
Lorenz Nimmervoll <admin@nimmervoll.work>
2023-03-10 13:27:16 +0100

    feat: init repo

---
+++ packages/eslint-config/react.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    '@atws/eslint-config',
+    'canonical/react',
+    'plugin:security/recommended',
+  ],
+  rules: {
+    // allows useEffect to be at the top
+    '@typescript-eslint/no-use-before-define': 'off',
+
+    // allows className and style as component props
+    'react/forbid-component-props': 'off',
+  },
+  settings: {
+    react: {
+      version: '18.0.0',
+    },
+  },
+}
