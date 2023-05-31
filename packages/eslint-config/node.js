df9d522b94a5f7154a90cd2d5340933c575bf8ae
Lorenz Nimmervoll <admin@nimmervoll.work>
2023-05-31 16:24:45 +0200

    fix(eslint-config): misconfigured rules for cdk and node

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -1,46 +1,4 @@
 module.exports = {
   extends: ['@atws/eslint-config', 'canonical/node'],
-  rules: {
-    // allows useEffect to be at the top
-    '@typescript-eslint/no-use-before-define': 'off',
-
-    // allow import of css files and locales
-    'import/no-unassigned-import': [
-      'error',
-      {
-        allow: [
-          '**/*.css',
-          '**/*.scss',
-          '**/*.sass',
-          '**/*.less',
-          'dayjs/locale/*',
-          'expo-router/entry',
-          'babel-register',
-        ],
-      },
-    ],
-
-    // allows className and style as component props
-    'react/forbid-component-props': 'off',
-
-    // conflict with prettier
-    'react/jsx-curly-newline': 'off',
-
-    // conflict with prettier
-    'react/jsx-indent': 'off',
-
-    // Disabled due to bug caused when using a type for the function handler
-    'react/prop-types': 'off',
-
-    // missing deps in hooks will display a warning instead of an error
-    'react-hooks/exhaustive-deps': 'warn',
-
-    // unnecessary in react
-    'security/detect-object-injection': 'off',
-  },
-  settings: {
-    react: {
-      version: '18.0.0',
-    },
-  },
+  rules: {},
 }
